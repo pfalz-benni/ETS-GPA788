0d286ce58a885d8d1d969beb71c8d68f753b3a7f
Benedikt Witteler <benedikt.witteler@tum.de>
2022-09-28 14:29:33 -0400

    Clean-up lab 2 by Alex

--- Lab2/test_lcd/test_lcd.ino
+++ Lab2/test_lcd/test_lcd.ino
@@ -20,6 +20,8 @@
  * de la température interne de l'ATmega328P est donnée dans les notes
  * de cours.
  */
+
+
 // Classe pour afficher des caractères à l'aide du LCD
 #include <LiquidCrystal.h>
 // Classe pour mesurer la température interne de l'ATmega328P
@@ -42,95 +44,107 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 // Créer un objet de type dht, une température et une humidité
 dht DHT;
-double dht_temp, dht_humi;
 
-// Relier le capteur à la broche #7
-const int DHT11_PIN{7};
+/*Variables et Constantes*/
+double dht_temp, dht_humi; //Création de variables afin de stocker et afficher les valeurs du DHT11
+const int DHT11_PIN{7}; // Relier le capteur à la broche #7
+const int16_t NB_MSG_COUNT{2}; //
+
+
 
 /* ---------------------------------------------------------------
    Fonction d'initialisation obligatoire (exécutée 1 seule fois).
    --------------------------------------------------------------- */
 void setup()
 {
-  // Pour afficher la température sur le terminal série
-  Serial.begin(115200);
+  //Fixe le débit de communication en nombre de caracères par 
+  //seconde(baud) pour la communication série
+    Serial.begin(115200);
 
   // Sur le VS Code, l'ouverture du port série prend du temps et on
   // peut perdre des caractères. Ce problème n'existe pas sur la carte
   // de l'Arduino.
-  waitUntil(2000);
+
+  // Attente de 2 secondes
+    waitUntil(2000);
 
   // Pour l'afficheur LCD
   // 0) Il s'agir d'un afficheur 16 x 2
-  lcd.begin(16, 2);
+    lcd.begin(16, 2);
   // 1) Effacer l'affichage
-  lcd.clear();
+    lcd.clear();
 
   // Lecture des données a jeter
   // TODO
 }
 
+
+
 /* ---------------------------------------------------------------
    Fonction principale
    (Exécutée comme une fonction dans une boucle sans fin)
    --------------------------------------------------------------- */
-const int16_t NB_MSG_COUNT{2};
 void loop()
 {
-  // Alterner entre le message de bienvenue et la température
-  // interne de l'ATmega328P.
+  // Alterner entre le message de bienvenue et les varibales du DHT11
   // messageCount: nombre de fois le contenu a été affiché
-  // showingMessage: true -> bienvenue, false -> température
-  static int16_t messageCount{0};
-  static bool showingMessage{true};
+  // showingMessage: true -> bienvenue, false -> température, humidité
+     static int16_t messageCount{0};
+     static bool showingMessage{true};
 
-  int chk = DHT.read11(DHT11_PIN);
+     int chk = DHT.read11(DHT11_PIN);
 
-  if (chk == DHTLIB_OK)
-  {
-    // affichage normal
-    dht_humi = DHT.humidity;
-    dht_temp = DHT.temperature;
-
-    // Si le compteur est à zéro alors change le contenu
-    if (messageCount == 0)
+  // Traitement du code de retour pour voir s'il est ok
+     if (chk == DHTLIB_OK)
     {
-      lcd.clear();
-      if (showingMessage)                            // était message de bienvenue? alors
-        showTemp((int)dht_temp, (int)dht_humi, lcd); // affiche la température interne.
-      else
-        welcome(lcd);
+     // Stockage des valeurs du DHT11
+        dht_humi = DHT.humidity;
+        dht_temp = DHT.temperature;
+
+     // Si le compteur est à zéro alors change le contenu
+        if (messageCount == 0)
+        {
+         // 
+            lcd.clear();
+            if (showingMessage) 
+            {                           // était message de bienvenue? alors
+                showTemp((int)dht_temp, (int)dht_humi, lcd); // affiche la température interne.
+            }
+            else
+            {
+                welcome(lcd);
+            }
+        }
+     // Si le compteur est > NB_MSG_COUNT alors remettre le compteur
+     // à zéro et change le contenu de l'afficheur.
+        if (messageCount++ > NB_MSG_COUNT)
+        {
+            messageCount = 0;
+            showingMessage = !showingMessage;
+        }
     }
-    // Si le compteur est > NB_MSG_COUNT alors remettre le compteur
-    // à zéro et change le contenu de l'afficheur.
-    if (messageCount++ > NB_MSG_COUNT)
+
+    else
     {
-      messageCount = 0;
-      showingMessage = !showingMessage;
+        // Afficher message d'erreur
+        Serial.print(F("Error: "));
+        Serial.println(chk);
+
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Error: ");
+        lcd.print(chk, 10);
     }
-  }
-  else
-  {
-    // Afficher message d'erreur
-    Serial.print("Error: ");
-    Serial.println(chk);
 
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Error: ");
-    lcd.print(chk, 10);
-  }
-
-  // Faire clignoter lentement l'afficheur
-  lcd.display();
-  waitUntil(2000);
-  lcd.noDisplay();
-  waitUntil(1000);
+    // Faire clignoter lentement l'afficheur
+    lcd.display();
+    waitUntil(2000);
+    lcd.noDisplay();
+    waitUntil(1000);
 }
 
 /* ---------------------------------------------------------------
    Fonction pour créer un delai de w millisecondes
-
    La fonction delay() est utilisée dans bien des tutoriel pour
    créer un delai temporel. On peut aussi créer notre propre délai
    et utiliser une unité de temps à notre guise.
@@ -161,25 +175,27 @@ void welcome(LiquidCrystal &l)
 }
 
 /* ---------------------------------------------------------------
-   Fonction pour afficher la température interne de l'ATmega328P.
+   Fonction pour afficher la température et l'humidité du DHT11.
    ---------------------------------------------------------------
    Note: On aurait pu utiliser l'objet global chipTemp directement.
    --------------------------------------------------------------- */
 void showTemp(int temp, int humi, LiquidCrystal &l)
 {
-  // D'abord le terminal série
-  Serial.print("Temperature : ");
+  // On afffiche les valeurs de la température et
+  // l'humidité sur le moniteur série
+  Serial.print(F("Temperature : "));
   Serial.println(temp);
-  Serial.print("Humidite : ");
+  Serial.print(F("Humidite : "));
   Serial.println(humi);
 
-  // Ensuite l'afficheur LCD
+  // On afffiche les valeurs de la température et
+  // l'humidité sur lcd
   l.setCursor(0, 0);
   l.print("Temp.: ");
   l.print(temp, 10);
   l.print((char)223);
   l.print("C");
-
+  
   l.setCursor(0, 1);
   l.print("Hum.: ");
   l.print(humi, 10);
